33f05fdaa50f46145fb86b712b10601cfeb5abf2
RezaAdi0105 <39455862+rezaadi0105@users.noreply.github.com>
2018-12-21 18:31:38 +0000

    Update anykernel.sh

--- zip/anykernel.sh
+++ zip/anykernel.sh
@@ -8,18 +8,20 @@ kernel.string=ancient kernel by whyredindonesia
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=0
+do.cleanuponabort=1
 device.name1=whyred
 device.name2=wayne
 device.name3=
 device.name4=
 device.name5=
+supported.sdk1=27
+supported.sdk2=28
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
-ramdisk_compression=auto;
+ramdisk_compression=gz;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
@@ -28,6 +30,7 @@ ramdisk_compression=auto;
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
+chmod -R 755 $ramdisk/sbin;
 chown -R root:root $ramdisk/*;
 
 ## AnyKernel install
