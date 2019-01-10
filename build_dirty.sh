dce2662ebb9ddfa79b5ae5797120253b4050e86a
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-01-11 01:28:37 +0700

    update 1.3 beta7

--- build_dirty.sh
+++ build_dirty.sh
@@ -19,7 +19,7 @@ KERNEL_DIR=$PWD
 REPACK_DIR=$KERNEL_DIR/zip
 OUT=$KERNEL_DIR/out
 ZIP_NAME="$VERSION"-"$DATE"
-VERSION="whyred-1.3-beta6"
+VERSION="whyred-1.3-beta7"
 DATE=$(date +%Y%m%d-%H%M)
 
 export KBUILD_BUILD_USER=WhyredIndo
