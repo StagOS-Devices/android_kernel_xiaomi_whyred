582cebec730b8bd86fc5797c72d74235b9946316
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-01-17 09:43:01 +0700

    Bringup 1.4

--- build_dirty.sh
+++ build_dirty.sh
@@ -19,7 +19,7 @@ KERNEL_DIR=$PWD
 REPACK_DIR=$KERNEL_DIR/zip
 OUT=$KERNEL_DIR/out
 ZIP_NAME="$VERSION"-"$DATE"
-VERSION="whyred-1.3-AospLos"
+VERSION="whyred-1.4-AospLos"
 DATE=$(date +%Y%m%d-%H%M)
 
 export KBUILD_BUILD_USER=builder
